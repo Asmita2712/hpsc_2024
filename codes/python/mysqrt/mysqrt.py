5ccb3e46c7089e40327869409028ad91502ccc76
Asmita Kamble <aakamble2000@gmail.com>
2024-02-15 20:19:16 +0530

    Corrected an error in convergence check, added check for special cases

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -2,6 +2,11 @@
 Module to calculate sqrt using Newtons Method
 """
 def sqrt2(x, debug=False):
+	from numpy import nan
+	if x==0:	
+		return 0
+	elif x<0:
+		return nan
 	s=1.0
 	kmax=100
 	tol=1.0e-14
@@ -11,7 +16,7 @@ def sqrt2(x, debug=False):
 		s0 = s
 		s = 0.5 * (s + (x/s))
 		delta_s = s-s0
-		if(abs(delta_s)/x < tol):
+		if(abs(delta_s/x) < tol):
 			break
 	if debug:
 		print(f"Finally, the value of s={s:20.15f}")
